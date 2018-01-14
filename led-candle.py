fd68f9847507b73f7dad551b6c25962bdb98973b
carefreetime1224 <carefreetime1224@gmail.com>
2018-01-14 13:43:40 +0000

    We have a real candle! YOOOOOOOO \ ^ ___ ^ /

---
+++ led-candle.py
@@ -0,0 +1,175 @@
+#include GPIO and Timer Library 
+import RPi.GPIO as GPIO
+import time
+import sys
+
+class led87_object(object):
+  def __init__(self):
+    #define Raspberry Pi GPIO number
+    self.sleeptime=0.001
+    self.ROW1=22
+    self.ROW2=18
+    self.ROW3=16
+    self.ROW4=35
+    self.ROW5=33
+    self.ROW6=31
+    self.ROW7=29
+    self.COL1=38
+    self.COL2=36
+    self.COL3=32
+    self.COL4=37
+    self.COL5=15
+    self.COL6=13
+    self.COL7=11
+    self.COL8=7
+
+    #Raspberry Pi GPIO initalization
+    GPIO.setmode( GPIO.BOARD )
+    GPIO.setup(self.COL1, GPIO.OUT)
+    GPIO.setup(self.COL2, GPIO.OUT)
+    GPIO.setup(self.COL3, GPIO.OUT)
+    GPIO.setup(self.COL4, GPIO.OUT)
+    GPIO.setup(self.COL5, GPIO.OUT)
+    GPIO.setup(self.COL6, GPIO.OUT)
+    GPIO.setup(self.COL7, GPIO.OUT)
+    GPIO.setup(self.COL8, GPIO.OUT)
+    GPIO.setup(self.ROW1, GPIO.OUT)
+    GPIO.setup(self.ROW2, GPIO.OUT)
+    GPIO.setup(self.ROW3, GPIO.OUT)
+    GPIO.setup(self.ROW4, GPIO.OUT)
+    GPIO.setup(self.ROW5, GPIO.OUT)
+    GPIO.setup(self.ROW6, GPIO.OUT)
+    GPIO.setup(self.ROW7, GPIO.OUT)
+
+    self.channel=12
+    GPIO.setup(self.channel, GPIO.IN)
+
+  def clear(self):
+    #set all GPIO output to LOW
+    GPIO.output(self.COL1, GPIO.LOW)
+    GPIO.output(self.COL2, GPIO.LOW)
+    GPIO.output(self.COL3, GPIO.LOW)
+    GPIO.output(self.COL4, GPIO.LOW)
+    GPIO.output(self.COL5, GPIO.LOW)
+    GPIO.output(self.COL6, GPIO.LOW)
+    GPIO.output(self.COL7, GPIO.LOW)
+    GPIO.output(self.COL8, GPIO.LOW)
+    GPIO.output(self.ROW1, GPIO.LOW)
+    GPIO.output(self.ROW2, GPIO.LOW)
+    GPIO.output(self.ROW3, GPIO.LOW)
+    GPIO.output(self.ROW4, GPIO.LOW)
+    GPIO.output(self.ROW5, GPIO.LOW)
+    GPIO.output(self.ROW6, GPIO.LOW)
+    GPIO.output(self.ROW7, GPIO.LOW)
+
+  def demo(self):
+
+    def callback(channel):
+      print "sound clap!"
+      self.clear()
+      sys.exit()
+      # if GPIO.input(channel):
+      #   print "Sound Detected!"
+      # else:
+      #   print "Sound yy!"
+
+    GPIO.add_event_detect(self.channel, GPIO.BOTH, bouncetime=2000)  
+    GPIO.add_event_callback(self.channel, callback) 
+
+    while 1:
+      self.clear()
+      GPIO.output(self.ROW1, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.HIGH)
+      GPIO.output(self.COL2, GPIO.HIGH)
+      GPIO.output(self.COL3, GPIO.HIGH)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.HIGH)
+      GPIO.output(self.COL6, GPIO.HIGH)
+      GPIO.output(self.COL7, GPIO.HIGH)
+      GPIO.output(self.COL8, GPIO.HIGH)
+      time.sleep(self.sleeptime)
+
+      self.clear()
+      GPIO.output(self.ROW2, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.HIGH)
+      GPIO.output(self.COL2, GPIO.HIGH)
+      GPIO.output(self.COL3, GPIO.HIGH)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.LOW)
+      GPIO.output(self.COL6, GPIO.HIGH)
+      GPIO.output(self.COL7, GPIO.HIGH)
+      GPIO.output(self.COL8, GPIO.HIGH)
+      time.sleep(self.sleeptime)
+
+      self.clear()
+      GPIO.output(self.ROW3, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.HIGH)
+      GPIO.output(self.COL2, GPIO.HIGH)
+      GPIO.output(self.COL3, GPIO.LOW)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.LOW)
+      GPIO.output(self.COL6, GPIO.LOW)
+      GPIO.output(self.COL7, GPIO.HIGH)
+      GPIO.output(self.COL8, GPIO.HIGH)
+      time.sleep(self.sleeptime)
+
+      self.clear()
+      GPIO.output(self.ROW4, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.HIGH)
+      GPIO.output(self.COL2, GPIO.LOW)
+      GPIO.output(self.COL3, GPIO.LOW)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.LOW)
+      GPIO.output(self.COL6, GPIO.LOW)
+      GPIO.output(self.COL7, GPIO.LOW)
+      GPIO.output(self.COL8, GPIO.HIGH)
+      time.sleep(self.sleeptime)
+
+      self.clear()
+      GPIO.output(self.ROW5, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.LOW)
+      GPIO.output(self.COL2, GPIO.LOW)
+      GPIO.output(self.COL3, GPIO.LOW)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.LOW)
+      GPIO.output(self.COL6, GPIO.LOW)
+      GPIO.output(self.COL7, GPIO.LOW)
+      GPIO.output(self.COL8, GPIO.LOW)
+      time.sleep(self.sleeptime)
+
+      self.clear()
+      GPIO.output(self.ROW6, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.LOW)
+      GPIO.output(self.COL2, GPIO.LOW)
+      GPIO.output(self.COL3, GPIO.LOW)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.LOW)
+      GPIO.output(self.COL6, GPIO.LOW)
+      GPIO.output(self.COL7, GPIO.LOW)
+      GPIO.output(self.COL8, GPIO.LOW)
+      time.sleep(self.sleeptime)
+
+      self.clear()
+      GPIO.output(self.ROW7, GPIO.HIGH)
+      GPIO.output(self.COL1, GPIO.HIGH)
+      GPIO.output(self.COL2, GPIO.LOW)
+      GPIO.output(self.COL3, GPIO.LOW)
+      GPIO.output(self.COL4, GPIO.LOW)
+      GPIO.output(self.COL5, GPIO.LOW)
+      GPIO.output(self.COL6, GPIO.LOW)
+      GPIO.output(self.COL7, GPIO.LOW)
+      GPIO.output(self.COL8, GPIO.HIGH)
+      time.sleep(self.sleeptime)
+      
+    self.clear()    
+
+    # while True:
+    #   time.sleep(1)
+
+def main():
+    ledobj=led87_object()
+    ledobj.demo()
+
+if __name__ == "__main__":
+    main()
+
